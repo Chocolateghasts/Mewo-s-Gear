a21d0c935f6a3c564145726b58f709c1b2b5e7fa
ChocolateMods <152035797+Chocolateghasts@users.noreply.github.com>
2025-11-29 12:24:38 +0100

    Idk what im doing

--- src/main/java/com/mewo/mewosgear/content/Item/SpecialSwordItem.java
+++ src/main/java/com/mewo/mewosgear/content/Item/SpecialSwordItem.java
@@ -2,6 +2,7 @@ package com.mewo.mewosgear.content.Item;
 
 import com.mewo.mewosgear.content.modifiers.IModifier;
 import com.mewo.mewosgear.content.modifiers.ModModifiers;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
@@ -10,6 +11,7 @@ import net.minecraft.world.item.Tier;
 import java.util.HashSet;
 import java.util.Set;
 
+import static com.mewo.mewosgear.Main.MOD_ID;
 import static com.mewo.mewosgear.content.modifiers.ModifierCategory.ONHIT;
 
 public abstract class SpecialSwordItem extends SwordItem implements IWeapon {
@@ -17,6 +19,8 @@ public abstract class SpecialSwordItem extends SwordItem implements IWeapon {
     private int maxModifierLevel;
     private int modifierLevel;
     private int testInt = 0;
+
+    private static final int MAX_MODIFIER_LEVEL = 5;
 
     public SpecialSwordItem(Tier tier, int dmg, float aspd, Properties properties) {
         super(tier, dmg, aspd, properties);
@@ -42,7 +46,10 @@ public abstract class SpecialSwordItem extends SwordItem implements IWeapon {
         return false;
     }
 
-    public Set<IModifier> getModifiers() {
+    public Set<IModifier> getModifiers(ItemStack stack) {
+        CompoundTag nbt = stack.getOrCreateTag();
+        CompoundTag modNbt = nbt.getCompound(MOD_ID);
+
         return Set.copyOf(modifiers);
     }
 
@@ -53,7 +60,7 @@ public abstract class SpecialSwordItem extends SwordItem implements IWeapon {
             if (testInt % 8 == 0) {
                 addModifier(ModModifiers.POISON_MODIFIER);
             }
-            for (IModifier modifier : getModifiers()) {
+            for (IModifier modifier : getModifiers(stack)) {
                 if (modifier.getCategory() == ONHIT) {
                     modifier.onHit(target);
                 }
@@ -61,6 +68,14 @@ public abstract class SpecialSwordItem extends SwordItem implements IWeapon {
             return true;
         }
         return false;
+    }
+
+    public int getMaxModifierLevel(ItemStack stack) {
+        CompoundTag nbt =  stack.getOrCreateTag();
+        CompoundTag modNbt = nbt.getCompound(MOD_ID);
+        int maxLevel = modNbt.getInt("MaxModifierLevel");
+        if (maxLevel > 0) return maxLevel;
+        return MAX_MODIFIER_LEVEL;
     }
 
     public void setMaxModifierLevel(int maxModifierLevel) {
