a85aadc88207e4dc46574b44289e70c91dc43be7
ChocolateMods <152035797+Chocolateghasts@users.noreply.github.com>
2026-06-12 18:50:46 +0200

    Cleaned up some stuff

--- src/main/java/com/mewo/mewosgear/content/misc/ExplosionVolatile.java
+++ src/main/java/com/mewo/mewosgear/content/misc/ExplosionVolatile.java
@@ -1,27 +1,27 @@
-package com.mewo.mewosgear.content.misc;
-
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.Explosion;
-import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
-
-public class ExplosionVolatile extends Explosion {
-    public ExplosionVolatile(Level level, @Nullable Entity source, double x, double y, double z, boolean isFire, float radius, List<BlockPos> positions) {
-        super(level, source, x, y, z, radius, positions);
-    }
-
-    @Override
-    public void explode() {
-        super.explode();
-        this.clearToBlow();
-
-    }
-
-    @Override
-    public void finalizeExplosion(boolean hasParticles) {
-        super.finalizeExplosion(hasParticles);
-    }
-}
+//package com.mewo.mewosgear.content.misc;
+//
+//import net.minecraft.core.BlockPos;
+//import net.minecraft.world.entity.Entity;
+//import net.minecraft.world.level.Explosion;
+//import net.minecraft.world.level.Level;
+//import org.jetbrains.annotations.Nullable;
+//
+//import java.util.List;
+//
+//public class ExplosionVolatile extends Explosion {
+//    public ExplosionVolatile(Level level, @Nullable Entity source, double x, double y, double z, boolean isFire, float radius, List<BlockPos> positions) {
+//        super(level, source, x, y, z, radius, positions);
+//    }
+//
+//    @Override
+//    public void explode() {
+//        super.explode();
+//        this.clearToBlow();
+//
+//    }
+//
+//    @Override
+//    public void finalizeExplosion(boolean hasParticles) {
+//        super.finalizeExplosion(hasParticles);
+//    }
+//}
